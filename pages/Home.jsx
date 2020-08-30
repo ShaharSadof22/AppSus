61004edc2969f0c88e7f3df26a0b402ccde53959
Shahar Sadof <shaharsadof22@gmail.com>
2020-08-30 10:12:26 +0300

    first commit2

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,8 +1,10 @@
 
 export function Home(){
 
+    const path = "../assets/img/homePic.jpg"
+
     return <div className="home-page flex column align-center">
-        <img src="../assets/img/homePic.jpg" />
+        <img src= {path} />
         <h1>Welcome to App<span>S</span>us</h1>
         <div className="subtitle-home flex"><p>Your Keep-Notes<span> & </span>Email Platform</p></div>
     </div>
